e8d1c6631dfa02f8ca3ccffdcd477a5eb0a8241a
David Karim <davidkarim@gmail.com>
2016-05-15 11:11:10 -0400

    Add migration for user avatar using paperclip.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160513232111) do
+ActiveRecord::Schema.define(version: 20160515140609) do
 
   create_table "samples", force: :cascade do |t|
     t.string   "name"
@@ -22,8 +22,12 @@ ActiveRecord::Schema.define(version: 20160513232111) do
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
